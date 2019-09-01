afb88dc4c3307de3ceec33e820fc7e642bf9aa25
Parth <parth1493@gmail.com>
2019-09-01 17:53:26 -0400

    Toolbar

--- app/src/main/java/com/test/materialdesignexample/MainActivity.java
+++ app/src/main/java/com/test/materialdesignexample/MainActivity.java
@@ -2,12 +2,28 @@ package com.test.materialdesignexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+
+        // will not support for above api 21
+//        getActionBar(toolbar);
+
+        //this will support below and above 21
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Welcome !");
+        toolbar.setSubtitle("Folks");
+        toolbar.setLogo(R.drawable.ic_launcher_background);
+        toolbar.setNavigationIcon(R.drawable.ic_launcher_background);
     }
 }
