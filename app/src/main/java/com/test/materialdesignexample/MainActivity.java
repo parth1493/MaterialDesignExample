4e133fa3241db14b4c1c8d6a4a6ffe9a92aec095
Parth <parth1493@gmail.com>
2019-09-01 18:16:58 -0400

    Added toolbar menu      - memu popup theme      - added java code to support it.

--- app/src/main/java/com/test/materialdesignexample/MainActivity.java
+++ app/src/main/java/com/test/materialdesignexample/MainActivity.java
@@ -3,6 +3,9 @@ package com.test.materialdesignexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,5 +30,43 @@ public class MainActivity extends AppCompatActivity {
 
 //        toolbar.setLogo(R.drawable.ic_launcher_background);
 //        toolbar.setNavigationIcon(R.drawable.ic_launcher_background);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        String msg = "";
+
+        switch (item.getItemId()) {
+
+            case R.id.discard:
+                msg = getString(R.string.delete);
+                break;
+
+            case R.id.search:
+                msg = getString(R.string.search);
+                break;
+
+            case R.id.edit:
+                msg = getString(R.string.edit);
+                break;
+
+            case R.id.settings:
+                msg = getString(R.string.settings);
+                break;
+
+            case R.id.Exit:
+                msg = getString(R.string.exit);
+                break;
+        }
+
+        Toast.makeText(this, msg + " clicked !", Toast.LENGTH_SHORT).show();
+
+        return super.onOptionsItemSelected(item);
     }
 }
